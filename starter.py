461c81f58ddbfdc2119c34d8c8175487e997e573
Wenjing Chen <wchen@research.baycrest.org>
2025-03-04 13:44:55 -0500

    add some comments to starter

--- starter.py
+++ starter.py
@@ -7,14 +7,15 @@ from rri_utilities.megprojector import RotmanMegProjector
 projector = RotmanMegProjector()
 meg_window = projector.window
 
-#initialise some stimuli
+#initialize stilmulus
 fixation = visual.Circle(meg_window, size = 10,
     lineColor = 'white', fillColor = 'lightGrey')
 
+# initialize magix pixel
 magic_pix = projector.make_magic_pixel(255, size_px=10)
 
+# present stimuli for 2 seconds
 frame_count = projector.convert_seconds_to_frames(2.0)
-
 for frame_number in range(frame_count):
     fixation.draw()
     magic_pix.draw()
