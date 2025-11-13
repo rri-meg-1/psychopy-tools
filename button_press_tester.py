38608c86a77eb4feaa476f766d84accd5768971d
Wenjing Chen <wchen@research.baycrest.org>
2025-11-13 16:55:02 -0500

    behavioral to csv and details to log

--- button_press_tester.py
+++ button_press_tester.py
@@ -1,15 +1,27 @@
 # vendor imports
-from psychopy import visual, event, core, data, gui
+import pandas as pd
+from psychopy import visual, event, core, logging
 from psychopy.hardware import keyboard
 
 # imports
 from rri_utilities.megprojector import RotmanMegProjector
 from rri_utilities.escaper import EscapeDetector
+
+
 projector = RotmanMegProjector()
 meg_window = projector.window
 
+log_file = logging.LogFile("data/lastRun.log", level=logging.INFO, filemode='w')
+
+# number of trials
+N_TRIALS = 100
+
 # initialize clock
 clock = core.Clock()
+
+# Reaction times
+# Pre-allocate data storage (no resizing during experiment)
+reaction_times = [0] * N_TRIALS
 
 # initialize keyboard
 kb = keyboard.Keyboard(clock=clock)
@@ -32,6 +44,14 @@ start_recording_magic_pix = projector.make_magic_pixel(255, size_px=10)
 start_trial_magic_pix = projector.make_magic_pixel(128, size_px=10)
 countdown_magic_pix = projector.make_magic_pixel(64, size_px=10)
 
+def flush_logs_and_exit():
+    """Flush logs and exit experiment."""
+    logging.flush()
+    meg_window.close()
+    df = pd.DataFrame(reaction_times)
+    df.to_csv('data/reaction_times.csv', index=False, header=False)
+    core.quit()
+
 COUNTDOWN_TIME_FRAMES = projector.convert_seconds_to_frames(2.0)
 def show_countdown():
     """Show countdown before trial starts."""
@@ -43,11 +63,11 @@ def show_countdown():
         meg_window.flip()
     # at the end of the countdown, check for escape press
     if escape_detector.has_pressed_escape():
-        core.quit()
+        flush_logs_and_exit()
         return
 
 MAX_TRIAL_TIME_FRAMES = projector.convert_seconds_to_frames(10.0)
-def show_trial():
+def show_trial(trial_number):
     """Show button press prompt until a button is pressed."""
     kb.clearEvents()
     start_trial_magic_pix.draw()
@@ -61,11 +81,17 @@ def show_trial():
         keys = kb.getKeys(keyList=allowed_keys, waitRelease=False)
         if keys:
             reaction_time = keys[0].rt
-            print(f"Button {keys[0].name} pressed at {reaction_time:.3f} seconds after frame {frame_number} onset.")
+            reaction_times[trial_number] = reaction_time
+            logging.log(
+                level=logging.EXP, 
+                msg=f"Button {keys[0].name} pressed at {reaction_time:.3f} seconds after frame {frame_number} (0-based) onset."
+            )
             return
         if escape_detector.has_pressed_escape():
             core.quit()
             return
+
+# main experiment loop
 
 # start screen
 fixation.draw()
@@ -74,16 +100,16 @@ meg_window.flip()
 
 escape_detector = EscapeDetector()
 
+# wait for experimenter to start
 event.waitKeys()
 
 
 # cycle through 100 trials
-for trial_number in range(100):
+for trial_number in range(N_TRIALS):
     show_countdown()
-    show_trial()
+    show_trial(trial_number)
     # inter-trial interval
-    core.wait(0.2)
+    core.wait(0.2) # seconds
 
 # clean-up
-meg_window.close()
-core.quit()
+flush_logs_and_exit()
